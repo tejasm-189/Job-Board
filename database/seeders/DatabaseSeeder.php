bb526417119b08e0e343058222e7a5935ec54d2f
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-18 11:45:40 +0530

    seed job data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        \App\Models\Job::factory(20)->create();
     }
 }
