578ce96b21be46694fef40904741a7a5918c3aac
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-19 12:46:26 +0530

    Create sign-in form

---
+++ resources/views/auth/create.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-section-heading>Log In</x-section-heading>
+
+    <form method="POST" action="/login" class="max-w-xl mx-auto space-y-6">
+        @csrf
+
+        <div class="flex flex-col gap-y-2">
+            <label for="email" class="font-bold text-sm text-black dark:text-white">Email</label>
+            <input type="email" name="email" id="email" required class="rounded-xl bg-white/5 border-white/10 px-5 py-4 border border-gray-200 dark:border-white/10 text-black dark:text-white placeholder-gray-500 focus:border-blue-600 focus:ring-blue-600 w-full">
+        </div>
+
+        <div class="flex flex-col gap-y-2">
+            <label for="password" class="font-bold text-sm text-black dark:text-white">Password</label>
+            <input type="password" name="password" id="password" required class="rounded-xl bg-white/5 border-white/10 px-5 py-4 border border-gray-200 dark:border-white/10 text-black dark:text-white placeholder-gray-500 focus:border-blue-600 focus:ring-blue-600 w-full">
+        </div>
+
+        <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-xl transition-colors w-full">Log In</button>
+    </form>
+</x-layout>
