7e2979c47fd13e5ed3453b724dee41dd5322b927
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-19 12:45:39 +0530

    Add AuthController and login/logout routes

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use App\Models\Job;
 Route::get('/', [App\Http\Controllers\JobController::class, 'index']);
 
 Route::get('/jobs/{job}', [App\Http\Controllers\JobController::class, 'show']);
+
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'create']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'store']);
+Route::delete('/logout', [App\Http\Controllers\AuthController::class, 'destroy']);
