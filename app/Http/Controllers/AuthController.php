981ed77b07bdaf105a93a67e84ed890b0ebec376
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-19 13:34:44 +0530

    Sign-in Logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -13,7 +15,20 @@ class AuthController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $attributes = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.',
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect('/');
     }
 
     public function destroy()
