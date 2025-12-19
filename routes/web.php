6e13b1af2fc45e728e5bf7bf7a9f288c333fd680
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-19 15:14:11 +0530

    feat: Name the login GET route.

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', [App\Http\Controllers\JobController::class, 'index']);
 
 Route::get('/jobs/{job}', [App\Http\Controllers\JobController::class, 'show']);
 
-Route::get('/login', [App\Http\Controllers\AuthController::class, 'create']);
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'create'])->name('login');
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'store']);
 Route::delete('/logout', [App\Http\Controllers\AuthController::class, 'destroy']);
 
